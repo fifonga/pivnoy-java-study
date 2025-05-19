784a1170d5e041824131d7b62a4a6e6cb0e8613b
fifonga <sewenander@gmail.com>
2025-05-20 00:45:44 +0700

    PIVO-108 Добавил логи в файлы AuthorController и AuthorServiceImpl

--- src/main/java/ttv/poltoraha/pivka/controller/AuthorController.java
+++ src/main/java/ttv/poltoraha/pivka/controller/AuthorController.java
@@ -1,6 +1,8 @@
 package ttv.poltoraha.pivka.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ttv.poltoraha.pivka.dao.request.AuthorDto;
 import ttv.poltoraha.pivka.entity.Author;
@@ -17,18 +19,27 @@ import java.util.List;
 public class AuthorController {
     private final AuthorService authorService;
 
+    private final Logger logger = LoggerFactory.getLogger(AuthorController.class);
+
+
     @PostMapping("/create")
     public void createAuthor(@RequestBody AuthorDto authorDto) {
+        logger.info("Получен запрос на /author/create");
         authorService.create(authorDto);
+        logger.info("Запрос на /author/create успешно выполнен");
     }
 
     @PostMapping("/delete")
     public void deleteAuthorById(@RequestParam Integer id) {
+        logger.info("Получен запрос на /author/delete. Удаление автора с ID={}", id);
         authorService.delete(id);
+        logger.info("Запрос на /author/delete успешно выполнен. Автор с ID={} удалён. ", id);
     }
 
     @PostMapping("/add/books")
     public void addBooksToAuthor(@RequestParam Integer id, @RequestBody List<Book> books) {
+        logger.info("Получен запрос на author/add/books. Добавление {} книг автору ID={}", books.size(), id);
         authorService.addBooks(id, books);
+        logger.info("Запрос на author/add/books успешно выполнен. Добавлено {} книг автору ID={}", books.size(), id);
     }
 }
--- src/main/java/ttv/poltoraha/pivka/serviceImpl/AuthorServiceImpl.java
+++ src/main/java/ttv/poltoraha/pivka/serviceImpl/AuthorServiceImpl.java
@@ -3,10 +3,13 @@ package ttv.poltoraha.pivka.serviceImpl;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ttv.poltoraha.pivka.controller.AuthorController;
 import ttv.poltoraha.pivka.dao.request.AuthorDto;
 import ttv.poltoraha.pivka.entity.Author;
 import ttv.poltoraha.pivka.entity.Book;
@@ -26,13 +29,21 @@ public class AuthorServiceImpl implements AuthorService {
     private final BookRepository bookRepository;
     private final BookService bookService;
 
+    private final Logger logger = LoggerFactory.getLogger(AuthorServiceImpl.class);
+
     // todo как будто надо насрать всякими мапперами
     @Override
     public void create(AuthorDto authorDto) {
+        logger.info("Вход в метод create() для Dto_имя: {}", authorDto.getFullName());
 
         if (authorDto.getBookId() != null) {
+            logger.info("Отправляется запрос к БД: bookRepository.findById() по BookId из Dto: {}", authorDto.getBookId());
             bookRepository.findById(authorDto.getBookId())
-                    .orElseThrow(() -> new EntityNotFoundException(String.format("Book with id = %d not found", authorDto.getBookId())));
+                    .orElseThrow(() -> {
+                        logger.warn("Книга с id ={} не найдена при создании автора", authorDto.getBookId());
+                        return new EntityNotFoundException(String.format("Book with id = %d not found", authorDto.getBookId()));
+                    });
+            logger.info("Получен ответ от БД: bookRepository.FindById() нашёл книгу по bookId из DTO: {}", authorDto.getBookId());
         }
 
         Author mappedAuthor = Author.builder()
@@ -40,54 +51,72 @@ public class AuthorServiceImpl implements AuthorService {
                 .avgRating(authorDto.getAvgRating())
                 .build();
 
+        logger.info("Отправляется запрос к БД: authorRepository.save() для создание автора с именем: {}", mappedAuthor.getFullName());
         authorRepository.save(mappedAuthor);
+        logger.info("Получен ответ от БД: authorRepository.save() автор создан с ID={}", mappedAuthor.getId());
     }
 
     @Override
     public void delete(Integer id) {
+        logger.info("Отправляется запрос к БД: authorRepository.deleteById() с ID: {}", id);
         authorRepository.deleteById(id);
+        logger.info("Получен ответ от БД: authorRepository.deleteById() с ID: {} выполнено", id);
     }
 
     @Override
     public void addBooks(Integer id, List<Book> books) {
         val author = getOrThrow(id);
 
+        logger.info("Пытается добавить в author с ID={} книги [{} книг]", id, books.size());
         author.getBooks().addAll(books);
+        logger.info("{} книг успешно добавлено в author с ID={}", books.size(), id);
     }
 
     @Override
     public void addBook(Integer id, Book book) {
         val author = getOrThrow(id);
 
+        logger.info("Пытается добавить в author с ID={} книгу: {}", id, book.getArticle());
         author.getBooks().add(book);
+        logger.info("Книга: [{}] успешно добавлена в author с ID={}",book.getArticle(), id);
     }
 
     @Override
     public List<Author> getTopAuthorsByTag(String tag, int count) {
         Pageable pageable = PageRequest.of(0, count);
+        logger.info("Пытается получить топ {} авторов по тегу: {}", count, tag);
         val authors = authorRepository.findTopAuthorsByTag(tag);
-
+        logger.info("Успешно получен топ {} авторов по тегу: {}", count, tag);
         return authorRepository.findTopAuthorsByTag(tag, pageable);
     }
 
     private Author getOrThrow(Integer id) {
+        logger.info("Отправляется запрос к БД: authorRepository.findById() с ID: {}", id);
         val optionalAuthor = authorRepository.findById(id);
+        logger.info("Получен ответ от БД: authorRepository.findById() с ID: {} выполнен", id);
         val author = optionalAuthor.orElse(null);
 
+        logger.info("Проверка author на null");
         if (author == null) {
+            logger.warn("author равен null");
             throw new RuntimeException("Author with id = " + id + " not found");
         }
+        logger.info("Проверка author на null прошла успешно");
 
         return author;
     }
 
     public void updateAuthor(Integer authorId, AuthorDto updatedData) {
+        logger.info("Отправляется запрос к БД: authorRepository.findById() с ID: {} на поиск автора", authorId);
         Author existingAuthor = authorRepository.findById(authorId)
                 .orElseThrow(() -> new EntityNotFoundException("Author not found with ID: " + authorId));
 
+        logger.info("Получен ответ от БД: authorRepository.findById() с ID: {} на поиск автора. Успешно", authorId);
         existingAuthor.setFullName(updatedData.getFullName());
         existingAuthor.setAvgRating(updatedData.getAvgRating());
 
+        logger.info("Отправляется запрос к БД: authorRepository.save() с ID: {} на обновление автора", authorId);
          authorRepository.save(existingAuthor);
+        logger.info("Получен ответ от БД: authorRepository.save() с ID: {} на обновление автора. Успешно", authorId);
     }
 }
