8c1517cf0c0f058ddb9b721b5cb70165997114bb
fifonga <sewenander@gmail.com>
2025-05-21 18:07:22 +0700

    PIVO-109 добавил метрики счётчика и таймера в AuthorController

--- src/main/java/ttv/poltoraha/pivka/metrics/CustomMetrics.java
+++ src/main/java/ttv/poltoraha/pivka/metrics/CustomMetrics.java
@@ -1,9 +1,43 @@
 package ttv.poltoraha.pivka.metrics;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 // Как правило все имеющиеся метрики создаются в отдельном классе.
 @Component
 public class CustomMetrics {
+    private final Counter myCounter;
+    private final Timer myTimer;
 
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    @Autowired
+    public CustomMetrics(MeterRegistry meterRegistry) {
+        this.myCounter = Counter
+                .builder("author_controller_count")
+                .description("total number of incoming requests")
+                .register(meterRegistry);
+
+
+        this.myTimer = meterRegistry.timer("db_call_duration", List.of(Tag.of("purpose", "db_calls")));
+    }
+
+    // методы для изменения метрик
+    public void recordMyCounter() {
+        myCounter.increment();
+    }
+
+    public void recordMyTimer(Long durationMs) {
+        myTimer.record(durationMs, TimeUnit.MILLISECONDS);
+    }
 }
